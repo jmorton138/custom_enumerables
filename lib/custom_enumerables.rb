99cd88fe4865f087b387efd78057c13a4ac11134
jmorton138 <jmorton138@gmail.com>
2021-07-20 09:40:08 +0100

    my_inject method

--- lib/custom_enumerables.rb
+++ lib/custom_enumerables.rb
@@ -88,10 +88,34 @@ module Enumerable
         end
         count
     end
+
+    def my_map
+        #yield(4)
+        new_array = []
+        for i in self
+            new_array << yield(i)
+        end
+        new_array
+    end
+
+    def my_inject(var = nil)
+        if var != nil
+            sum = var
+        else
+            sum = self[0]  
+        end
+        self.my_each_with_index do |i, index|
+            unless index == 0 && var == nil
+                sum = yield(sum, i)
+            end 
+        end
+        sum
+    end
 end
 
+
 #puts "my_each vs. each"
-numbers = [1, 3, "steve", 4, 3, 6]
+numbers = [1, 2, 3, 4, 5, 6]
 # numbers.my_each { |item| puts item }
 # numbers.each { |item| puts item}
 
@@ -113,4 +137,13 @@ numbers = [1, 3, "steve", 4, 3, 6]
 # puts numbers.select { |item| item >= 2 }
 
 #puts numbers.count(3)
-puts numbers.my_count("steve")
+#p numbers.my_map {|item| item + 2}
+#puts numbers.map {|item| item + 1}
+
+puts numbers.inject {|sum, num| sum + num}
+puts numbers.my_inject {|sum, num| sum + num}
+
+
+
+
+
